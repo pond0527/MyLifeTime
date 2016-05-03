f871b2c330d28a4a1b825b7d4edc284e21936ab3
pond0527 <like4869oge@yahoo.co.jp>
2016-05-03 19:49:46 +0900

    画面名：MyLifeTimeプロジェクト 機能名：Baseクラス 内容：ラベルエフェクト等のライブラリ適用。エンティティクラスの作成。

--- MyLifeTime/Controller/Base/BaseViewController.swift
+++ MyLifeTime/Controller/Base/BaseViewController.swift
@@ -7,8 +7,9 @@
 //
 
 import UIKit
+import SwiftDate
 
-class BaseViewController: UIViewController {
+class BaseViewController: UIViewController, LTMorphingLabelDelegate, UITableViewDelegate, UIViewControllerTransitioningDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +20,62 @@ class BaseViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    /**
+     自クラス呼び出し自に処理されます。
+     
+     - parameter animated: <#animated description#>
+     */
+    override func viewDidAppear(animated: Bool) {
+    }
 
+    /**
+     メッセージを表示します。
+     
+     - parameter titleName: タイトル名
+     - parameter msg:       本文
+     */
+    func showMessage(titleName: String, msg: String) {
+        
+        let msgAlert = UIAlertController(title: titleName, message: msg, preferredStyle: .Alert)
+        let defaultAction = UIAlertAction(title: "OK", style: .Default, handler:{(action:UIAlertAction!) -> Void in })
+        msgAlert.addAction(defaultAction)
+        
+        willMoveToParentViewController(self)
+        
+        //メッセージ表示
+        presentViewController(msgAlert,animated: true, completion: nil)
+    }
+    
+    /**
+     画面タッチでキーボードを閉じる。
+     */
+    @IBAction func tapScrn(sender: AnyObject) {
+        
+        //        txtName.resignFirstResponder()
+        //        txtPhone.resignFirstResponder()
+    }
 
+    /**
+     遅延処理を行います。
+     
+     - parameter time:  遅延時間
+     - parameter block: アクション
+     */
+    func wait_atleast(time : NSTimeInterval, @noescape _ block: () -> Void) {
+        
+        let start = CFAbsoluteTimeGetCurrent()
+        block()
+        let end = CFAbsoluteTimeGetCurrent()
+        let wait = max(0.0, time - (end - start))
+        if wait > 0.0 {
+            NSThread.sleepForTimeInterval(wait)
+        }
+    }
+    
+    
+    func presentationControllerForPresentedViewController(presented: UIViewController, presentingViewController presenting: UIViewController, sourceViewController source: UIViewController) -> UIPresentationController? {
+        return UIPresentationController(presentedViewController: presented, presentingViewController: presenting)
+    }
 }
 
--- MyLifeTime/Entity/Person.swift
+++ MyLifeTime/Entity/Person.swift
@@ -7,3 +7,121 @@
 //
 
 import Foundation
+import RealmSwift
+
+/// ユーザエンティティを管理するクラスです。
+class Person: Object {
+    static let realm = try! Realm()
+    
+    dynamic var id = 0
+    dynamic var nm = ""
+    dynamic var sex = ""
+    dynamic var year = ""
+    dynamic var month = ""
+    dynamic var day = ""
+    dynamic var bondSts = false
+    
+    /*主キー*/
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+    
+    /**
+     ユーザの新規作成を行います。
+     */
+    static func create() -> Person {
+        let user = Person()
+        user.id = lastId()
+        return user
+    }
+    
+    /**
+     ユーザIDを採番します。
+     */
+    static func lastId () -> Int {
+        if let user = realm.objects(Person).last {
+            return user.id + 1
+        } else {
+            return 1
+        }
+    }
+    
+    /**
+     全ユーザ情報を取得します。
+     */
+    static func loadAll() -> [Person] {
+        let userList = realm.objects(Person).sorted("id", ascending: true)
+        var rslt: [Person] = []
+        for user in userList {
+            rslt.append(user)
+        }
+        return rslt
+    }
+
+    /**
+     保存処理を行います。
+     */
+    func save() {
+        try! Person.realm.write {
+            Person.realm.add(self)
+        }
+    }
+    
+    /**
+     更新処理を行います。
+     */
+    static func update(method: (() -> Void)) {
+        try! Person.realm.write {
+            method()
+        }
+    }
+
+    /**
+     フィルター検索を行います。
+     
+     - parameter srchNm:    ユーザ名
+     - parameter srchSex:   性別
+     - parameter srchPhone: 電話番号
+     
+     - returns: <#return value description#>
+     */
+    static func searchUsers(srchNm: String?, srchSex: String?, srchPhone: String?) -> Results<Person> {
+        
+        //検索条件をNSPredicateで作成
+        let predicate = NSCompoundPredicate(type: NSCompoundPredicateType.OrPredicateType
+            , subpredicates: [
+                NSPredicate(format: "nm = %@", srchNm!),
+                NSPredicate(format: "sex = %@", srchSex!),
+                NSPredicate(format: "phone = %@", srchPhone!),
+            ])
+        
+        let result = realm.objects(Person).filter(predicate)
+        return result
+    }
+    
+    /**
+     全ユーザ情報を削除します。
+     */
+    static func deleteAll() {
+        
+        // 指定されたセルのオブジェクトをユーザ情報から削除
+        try! realm.write {
+            realm.deleteAll()
+        }
+    }
+    
+    /**
+     ユーザIDを指定してユーザを削除します。
+     
+     - parameter id: ユーザID
+     */
+    static func delete(id: Int?) {
+        
+        // 指定されたセルのオブジェクトをユーザ情報から削除
+        let predicate = NSPredicate(format: "id = %d", id!)
+        let user = realm.objects(Person).filter(predicate)
+        try! realm.write {
+            realm.delete(user)
+        }
+    }
+}
